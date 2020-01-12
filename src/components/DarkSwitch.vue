b6331401cfeeeb5860cc6f40ee8fd1e8f4864ada
TheTechdoodle <admin+github@darkender.com>
2020-01-12 15:22:37 -0800

    Begin work on interface

---
+++ src/components/DarkSwitch.vue
@@ -0,0 +1,25 @@
+<template>
+        <v-switch v-model="$vuetify.theme.dark" :append-icon="icon" hide-details label="Dark Mode"/>
+</template>
+
+<script>
+    export default {
+        name: 'DarkSwitch',
+        computed: {
+            icon()
+            {
+                return 'mdi-' + (this.$vuetify.theme.dark ? 'weather-night' : 'white-balance-sunny');
+            }
+        },
+        watch: {
+            '$vuetify.theme.dark': function(dark)
+            {
+                //TODO save dark mode
+            }
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>
